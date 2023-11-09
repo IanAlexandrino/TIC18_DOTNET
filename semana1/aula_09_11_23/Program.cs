31370be193e295fada058bd4119ac1d70e3f0089
Ian <ianrodrigues2001@hotmail.com>
2023-11-09 17:19:00 -0300

    Adicionando mais exemplos

--- semana1/aula_09_11_23/Program.cs
+++ semana1/aula_09_11_23/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Hello");
+﻿Console.WriteLine("Bom dia!!!");
 
 int a = 2;
 int b = 5;
@@ -10,3 +10,31 @@ Console.WriteLine(c);
 teclado = Console.ReadLine();
 
 Console.WriteLine(teclado);
+
+#region Teste de tipos de dados
+
+    int tipoInteiro;
+    double tipoDouble;
+    string tipoString;
+    long tipoLong; 
+
+    tipoInteiro = int.MaxValue;
+    tipoDouble = double.MaxValue;
+    tipoLong = long.MaxValue;
+    tipoString = "100";
+
+    tipoInteiro = int.Parse(tipoString);
+
+    Console.WriteLine(tipoInteiro);
+    Console.WriteLine(tipoDouble);
+    Console.WriteLine(tipoLong);
+
+#endregion
+
+#region Operadores
+
+    tipoDouble = tipoInteiro + tipoLong;
+
+    tipoInteiro = 10 > 5 ? 1 : 0;
+
+#endregion
